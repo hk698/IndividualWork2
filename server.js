7f0f2201460c578740fdaebe4084a71e6f580c8b
hk698 <khanhasnaat111@gmail.com>
2021-01-21 16:58:11 +0000

    updates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
