1c3f271d6c02e1ebea6bb9d3dd866edae564798b
hk698 <khanhasnaat111@gmail.com>
2021-01-21 17:21:37 +0000

    new updates

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 app.use((request, respond, next) =>{
     console.log(request.url);
